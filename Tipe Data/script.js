b4d7ecd802a539cef0269525f9722c2aa26d8ba4
niel2512 <nielyusuf17@gmail.com>
2025-06-27 19:52:09 +0700

    Data Types

---
+++ Tipe Data/script.js
@@ -0,0 +1,27 @@
+// String
+let nama = "Niel";
+console.log(nama);
+console.log(typeof nama);
+// Overloading
+nama = 1;
+console.log(nama);
+console.log(typeof nama);
+
+// Number
+let nomor = 3.14;
+console.log(nomor);
+console.log(typeof nomor);
+
+// Boolean
+let bool = true;
+console.log(bool);
+console.log(typeof bool);
+
+// Undefined
+let data;
+console.log(data);
+console.log(typeof data);
+// Overloading
+data = "nels";
+console.log(data);
+console.log(typeof data);
