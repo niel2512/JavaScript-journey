66abf2c17a6b28dc67c5f273ee1f461a5438e69c
niel2512 <nielyusuf17@gmail.com>
2025-07-05 14:38:47 +0700

    Komparasi

---
+++ Operasi Komparasi/script.js
@@ -0,0 +1,28 @@
+// Operator Komparasi
+// == equality
+// != inequality
+// === strict equality
+// !== strict inequality
+// > lebih besar dari
+// >= lebih besar sama dengan
+// <=lebih kecil sama dengan
+
+console.log(`7 > 10 = ${7 > 10}`);
+let dataBool = (10 > 7);
+console.log(`10 > 7 = ${dataBool}`);
+
+console.log(`10 >= 10 = ${10 >= 10}`);
+console.log(`7 < 10 = ${7 < 10}`);
+console.log(`7 <= 10 = ${7 <= 10}`);
+
+// Persamaan
+console.log(`3 == "3" = ${3 == "3"}`);
+console.log(`3 != "3" = ${3 != "3"}`);
+
+console.log(`3 === 3 = ${3 === 3}`);
+console.log(`3 === "3" = ${3 === "3"}`);
+console.log(`"3" === "3" = ${"3" === "3"}`);
+
+console.log(`3 !== 3 = ${3 !== 3}`);
+console.log(`3 !== "3" = ${3 !== "3"}`);
+console.log(`"3" !== "3" = ${"3" !== "3"}`);
