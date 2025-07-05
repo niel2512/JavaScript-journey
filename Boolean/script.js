be22635290daef509579f48227f90c144e44bcad
niel2512 <nielyusuf17@gmail.com>
2025-07-05 08:03:04 +0700

    Boolean

---
+++ Boolean/script.js
@@ -0,0 +1,21 @@
+// tipe data boolean
+let data_bool = true; //binary true = 1 false = 0
+console.log(data_bool);
+
+let data_bool2 = (3 < 10);
+console.log(data_bool2);
+
+let data_bool3 = ("niel" == "nyl");
+console.log(data_bool3);
+
+let data_bool4 = (true == 1);
+console.log(data_bool4);
+
+data_bool4 = (false == 0.0);
+console.log(data_bool4);
+
+data_bool4 = (false == 0n);
+console.log(data_bool4);
+
+data_bool4 = (false == " ");
+console.log(data_bool4);
