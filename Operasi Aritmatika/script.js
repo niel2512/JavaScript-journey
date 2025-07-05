3434c7a188073c90b9d1c82ad32fedc2e259d303
niel2512 <nielyusuf17@gmail.com>
2025-07-05 12:56:49 +0700

    Aritmatika

---
+++ Operasi Aritmatika/script.js
@@ -0,0 +1,34 @@
+// Operasi Aritmatika
+
+// Ekspresi (expression) dan statement
+1 + 1 //ini ekspresi
+a = 1 + 1 //ini statement
+
+// % = Modulo, ++ = Increment, -- = Decrement, ** = Pangkat
+
+let x = 10
+console.log(x + 1.5);
+console.log(x - 1.5);
+console.log(x / 1.5);
+console.log(x * 1.5);
+
+// Aritmatika Khusus JS
+console.log(x % 4);
+console.log(x ** 2);
+
+// pemrograman biasa 
+x = x + 1;
+console.log(x);
+x++
+console.log(x);
+
+x = x - 1;
+console.log(x);
+x--;
+console.log(x);
+
+// studi kasus
+let y = 1 + "1";
+console.log(y);
+y = 1 + parseInt("1");
+console.log(y);
