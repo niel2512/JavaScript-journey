a42361b284e863c07b2700071fe096b04553d97b
niel2512 <nielyusuf17@gmail.com>
2025-07-11 22:16:01 +0700

    IF Statement

---
+++ Percabangan/script.js
@@ -0,0 +1,10 @@
+let nama = "nyl";
+
+console.log("Nama" + " = " + nama);
+
+if (nama === "Niel") {
+  console.log("Halo Niel")
+} else {
+  console.log("Nama tidak sama")
+}
+console.log("Akhir Dari Program"); //seperti blok finnaly di java yang selalu dijalankan apapun hasil setelah blok sebelumnya dijalankan
