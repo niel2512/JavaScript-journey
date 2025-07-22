e41cfa720a7d62465a249e242f171096521db131
niel2512 <nielyusuf17@gmail.com>
2025-07-22 09:24:53 +0800

    Latihan For and While

---
+++ Pengulangan/Latihan2/script.js
@@ -0,0 +1,10 @@
+let jmlAngkot = 10
+let angkotBeroperasi = 8
+let noAngkot = 1
+while (noAngkot <= angkotBeroperasi) {
+  console.log("Angkot No. " + noAngkot + " beroperasi dengan baik.")
+  noAngkot++
+}
+for (noAngkot = angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++) {
+  console.log("Angkot No. " + noAngkot + " sedang tidak beroperasi.")
+}
