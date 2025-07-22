46f8cb3d2918884ef008a4744e9bff7b190abcab
niel2512 <nielyusuf17@gmail.com>
2025-07-22 08:35:12 +0800

    Looping While

---
+++ Pengulangan/while/script.js
@@ -0,0 +1,11 @@
+// let ulang = true
+// while (ulang === true) {
+//   console.log("hello world!");
+//   ulang = confirm("lagi?");
+// }
+
+let nilaiAwal = 1
+while (nilaiAwal <= 10) {
+  console.log("hello world " + nilaiAwal + 'x');
+  nilaiAwal++;
+}
