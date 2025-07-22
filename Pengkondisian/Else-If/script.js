7d0de358a4d9f5a2219c1f98b5cafb5e79e8365a
niel2512 <nielyusuf17@gmail.com>
2025-07-22 11:27:24 +0800

    Else-If

---
+++ Pengkondisian/Else-If/script.js
@@ -0,0 +1,9 @@
+let angka = prompt("Masukkan angka: ");
+
+if (angka % 2 === 0) {
+  alert(angka + " Adalah bilangan Genap")
+} else if (angka % 2 === 1) {
+  alert(angka + " Adalah bilangan Ganjil")
+} else {
+  alert("Yang anda masukkan bukan angka!")
+}
